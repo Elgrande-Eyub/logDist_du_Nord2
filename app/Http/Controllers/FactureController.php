c9a5939d8fb566504c1ded13af4d5bfdba07fd1d
Elgrande-Eyub <ayoubfcb2002@gmail.com>
2023-06-17 00:46:54 +0100

    update

--- app/Http/Controllers/FactureController.php
+++ app/Http/Controllers/FactureController.php
@@ -302,10 +302,10 @@ class FactureController extends Controller
                 $articles[] = $article;
             }
 
-          return   $factures = facture::join('bon_livraisons', 'factures.bonLivraison_id', '=', 'bon_livraisons.id')
-            ->join('fournisseurs', 'factures.fournisseur_id', '=', 'fournisseurs.id')
-            ->join('bon_commandes', 'bon_livraisons.bonCommande_id', '=', 'bon_commandes.id')
-            ->select('factures.*', 'bon_livraisons.Numero_bonLivraison', 'fournisseurs.fournisseur as fournisseur', 'bon_commandes.Numero_bonCommande as Numero_bonCommande')
+             $factures = facture::leftjoin('bon_livraisons', 'factures.bonLivraison_id', '=', 'bon_livraisons.id')
+            ->leftjoin('fournisseurs', 'factures.fournisseur_id', '=', 'fournisseurs.id')
+            ->leftjoin('bon_commandes', 'bon_livraisons.bonCommande_id', '=', 'bon_commandes.id')
+            ->select('factures.*', 'bon_livraisons.Numero_bonLivraison', 'fournisseurs.fournisseur as fournisseur', 'bon_commandes.Numero_bonCommande')
             ->where('factures.id', $id)
             ->first();
 
